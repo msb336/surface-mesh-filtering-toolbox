8e01dd7217bb649c22e7722ebe5d70f3854effd5
msb336 <32399587+msb336@users.noreply.github.com>
2018-01-08 14:45:49 -0500

    clusted bridge data
    added functionality to pick apart point cloud data and process each segment as a mesh.

--- makeHollow.m
+++ makeHollow.m
@@ -3,24 +3,29 @@ function [ newpoints ] = makeHollow( pts )
 %  Pseudocode:
 %  for point in points
 %       check if point is local max x,y, or z. if it is, keep it.
-thresh = 0.1*max(pts(:));
-% d = [ -thresh thresh ];
-% [x,y,z] = meshgrid(d,d,d);
-% cube = [x(:) y(:) z(:)];
-newpoints = [];
+% thresh = 0.1*max(pts(:));
+% 
+% newpoints = [];
+% 
+% for i = 1:length(pts)
+%     center = pts(i,:);
+%     temp = pts - center;
+%     inside = pts(sqrt(sum(temp.^2, 2)) <= thresh, :);
+%     
+%     posedges = [max(inside(:,1)) max(inside(:,2)) max(inside(:,3))];
+%     negedges = [min(inside(:,1)) min(inside(:,2)) min(inside(:,3))];
+%     
+%     if any(center == posedges) || any(center == negedges)
+%         newpoints(end+1,:) = center;
+%     end
+% end
 
-for i = 1:length(pts)
-    center = pts(i,:);
-    temp = pts - center;
-    inside = pts(sqrt(sum(temp.^2, 2)) <= thresh, :);
-    
-    posedges = [max(inside(:,1)) max(inside(:,2)) max(inside(:,3))];
-    negedges = [min(inside(:,1)) min(inside(:,2)) min(inside(:,3))];
-    
-    if any(center == posedges) || any(center == negedges)
-        newpoints(end+1,:) = center;
-    end
-end   
+centroid = mean(pts);
+angle1 = 0:def:pi;
+angle2 = 0:def:pi;
+
+
+
 
 end
 
--- meshfiltertest.m
+++ meshfiltertest.m
@@ -1,18 +1,21 @@
 clearvars;clc;
-addpath functions
+addpath functions data
+
 %%
-shape = 'l';
+shape = 'read';
 shape = lower(shape);
 noise_level = 0;
 definition = 0.2;
 %%
 d = [ -1:definition:1 ];
-[x1,y1,z1] = meshgrid(d,d,d);
+
 switch shape
     case 'l'
         %% L
+        [x1,y1,z1] = meshgrid(d,d,d);
         [x2,y2,z2] = meshgrid(d,d,d+2);
         [x3,y3,z3] = meshgrid(d,d+2,d);
+        
         x = [x1(:); x2(:); x3(:)];
         y = [y1(:); y2(:); y3(:)];
         z = [z1(:); z2(:); z3(:)];
@@ -21,38 +24,49 @@ switch shape
         %% Sphere
         [x,y,z] = sphere(ceil(100/definition));
         p = unique([x(:), y(:), z(:)],'rows');
+    case 'read'
+        points = csvread('bridge.csv');
+
 end
 
+%% k-means cluster
+K = 5;
+chunk = points(1:50000,:);
+[G, C] = kmeans(chunk, K);
 
+for i = 1:K
+%% 
+p = points(G==i,:);
 %% Noise
-p = makeHollow(p);
 pnoise = p + noise_level*(randn(size(p))-0.5);
+% figure;scatter3(p(:,1), p(:,2), p(:,3), '.')
 
 
 %% Triangulation
 tri = delaunayTriangulation(pnoise);
 
 %% Apply Constraints
-constraints.Length = findNearest(pnoise,5);
+constraints.Length = findNearest(pnoise,15);
 constraints.Angle = 0;
 conTri = constrain(tri, constraints);
 
 %% Ground Truth
-ctri = delaunayTriangulation(p);
-ccon = constrain(ctri, constraints);
-%% Plot Triangulation
-figure; h1 = trisurf(conTri);view(-108,20);title('No Smoothing');
-axis equal
+% ctri = delaunayTriangulation(p);
+% ccon = constrain(ctri, constraints);
+% %% Plot Triangulation
+% figure; h1 = trisurf(conTri);view(-108,20);title('No Smoothing');
+% axis equal
 %% Isotropic Gaussian Fairing
 smooth = isoLaplace(conTri);
 %%
-figure;
-h2 = trisurf(smooth);
-h2.FaceColor = 'None';
+trisurf(smooth);
+hold on
+
+end
+
 view(-108,20);
 title('Smoothed');
 axis equal
-
 %% Mean Curvature Flow
 
 %% Shape Error
